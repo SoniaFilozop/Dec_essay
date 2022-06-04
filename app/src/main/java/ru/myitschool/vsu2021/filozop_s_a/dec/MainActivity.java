f155458d6d5e829e211a286a95cc2eb58aa8f5e8
SoniaFilozop <soniafilozop20012005@gmail.com>
2022-06-04 08:34:09 +0300

    Commit

--- app/src/main/java/ru/myitschool/vsu2021/filozop_s_a/dec/MainActivity.java
+++ app/src/main/java/ru/myitschool/vsu2021/filozop_s_a/dec/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -30,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent alarmIntent = new Intent(this, CallDataSend.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0,
+                alarmIntent, PendingIntent.FLAG_IMMUTABLE);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 60000, pendingIntent);
+        Toast.makeText(this, "Привет!", Toast.LENGTH_LONG).show();
+
         transfer = (Button) findViewById(R.id.transfer);
         transfer1 = (Button) findViewById(R.id.transfer1);
         transfer2 = (Button) findViewById(R.id.transfer2);
